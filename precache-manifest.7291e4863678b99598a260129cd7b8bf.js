b7f826926b71210e10e7cd8c225c8410026bfa1a
MayurMallavSaikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-19 12:24:55 +0530

    Updates

---
+++ precache-manifest.7291e4863678b99598a260129cd7b8bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da0bf72f6cba5bb90222baffda4c26f",
+    "url": "/reactMovieClient/index.html"
+  },
+  {
+    "revision": "a1c75b60367bc6034e9b",
+    "url": "/reactMovieClient/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "b0f52bc973cba4615fff",
+    "url": "/reactMovieClient/static/css/main.27384cf9.chunk.css"
+  },
+  {
+    "revision": "a1c75b60367bc6034e9b",
+    "url": "/reactMovieClient/static/js/2.dd7d4a43.chunk.js"
+  },
+  {
+    "revision": "b0f52bc973cba4615fff",
+    "url": "/reactMovieClient/static/js/main.86d0fe93.chunk.js"
+  },
+  {
+    "revision": "c368b4440e7809ba310c",
+    "url": "/reactMovieClient/static/js/runtime~main.203b1d11.js"
+  }
+]);
