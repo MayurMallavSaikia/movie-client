fc75ba2ec32a58223451cdf8d51e80eef027c911
MayurMallavSaikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-19 10:58:39 +0530

    second commit

---
+++ build/precache-manifest.8168bb1852ec18022b8a29fd0b9e3b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4295c5bdaa24a798d89ac05092852a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f53009e079f3822bbddd",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "ba3c6313feac13c65521",
+    "url": "/static/css/main.27384cf9.chunk.css"
+  },
+  {
+    "revision": "f53009e079f3822bbddd",
+    "url": "/static/js/2.7cdcee44.chunk.js"
+  },
+  {
+    "revision": "ba3c6313feac13c65521",
+    "url": "/static/js/main.5d9fd2f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
