4615fbe1bae1908f33f73ae3e25078a25e674082
MayurMallavSaikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-19 11:34:38 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Router, Route, Routes , Switch} from 'react-router-dom';
 import Auth from "../hoc/auth";
 // pages for this product
 import LandingPage from "./views/LandingPage/LandingPage.js";
@@ -9,19 +9,23 @@ import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 import MovieDetail from "./views/MovieDetail/MovieDetail"
 import FavoritePage from "./views/FavoritePage/FavoritePage"
+import { Error404 } from './views/Error/Error404';
 function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
+      <BrowserRouter>
       <NavBar />
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
-        <Switch>
-          <Route exact path="/reactMovieClient" component={Auth(LandingPage, null)} />
-          <Route exact path="/reactMovieClient/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/reactMovieClient/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/reactMovieClient/movie/:movieId" component={Auth(MovieDetail, null)} />
-          <Route exact path="/reactMovieClient/favorite" component={Auth(FavoritePage, null)} />
-        </Switch>
+        <Routes>
+          <Route path="/reactMovieClient" component={Auth(LandingPage, null)} />
+          <Route path="/reactMovieClient/login" component={Auth(LoginPage, false)} />
+          <Route path="/reactMovieClient/register" component={Auth(RegisterPage, false)} />
+          <Route path="/reactMovieClient/movie/:movieId" component={Auth(MovieDetail, null)} />
+          <Route path="/reactMovieClient/favorite" component={Auth(FavoritePage, null)} />
+          <Route path="*" element={<Error404/>} />
+        </Routes>
       </div>
+      </BrowserRouter>
       <Footer />
     </Suspense>
   );
